8e78ea602a816d026a3c53f102762dd3492034d7
DavDev <dawtom97@gmail.com>
2021-06-10 08:27:16 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react';
+import "./App.css";
+import MovieList from './components/MovieList';
+
+
+const App = () => {
+
+  return ( 
+   <div>
+    
+   </div>
+   );
+}
+ 
+export default App;
